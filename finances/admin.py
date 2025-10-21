b8e28ef686be80dab617d06589a1dce6e3548e2d
Nikita <nikitamartynenko555@gmail.com>
2025-10-21 21:29:47 +0200

    admin: register finances models with filters, search and ordering

--- finances/admin.py
+++ finances/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Table, Category, Transaction
 
-# Register your models here.
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'created_at')
+    search_fields = ('name', 'owner__email')
+    list_filter = ('created_at',)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'table', 'amount', 'currency', 'date')
+    search_fields = ('title', 'table__name', 'table__owner__email')
+    list_filter = ('currency', 'date', 'categories')
+    ordering = ('-date',)
